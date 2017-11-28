a190d21cad15208df8945ea7dd6550926e5836cb
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-11-28 17:58:56 +0100

    Update

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class UserController extends AbstractController
 {
+    /**
+     * @Route("/user", name="user")
+     */
+    public function test()
+    {
+        $user = new User();
 
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create User'))
+            ->getForm();
+
+        return $this->render("test.html.twig", [
+            'form' => $form->createView(),
+        ]);
+    }
 }
